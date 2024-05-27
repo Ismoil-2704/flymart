5929461294f6e802654a7253deafed74c334dcb0
Ismoil-2704 <anvaroismoil@gmail.com>
2024-05-27 17:52:29 +0500

    transaction boshlandi 11 task...

--- src/main/java/com/example/flymart/entity/Product.java
+++ src/main/java/com/example/flymart/entity/Product.java
@@ -1,11 +1,12 @@
 package com.example.flymart.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -15,4 +16,14 @@ import lombok.Setter;
 public class Product extends BaseEntity{
     @Column(name = "name")
     private String name;
+    @ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST,  CascadeType.REFRESH })
+    @JoinTable(name = "product_request",
+            joinColumns = {@JoinColumn(name = "product_id")},
+            inverseJoinColumns = {@JoinColumn(name = "request_id")})
+    private List<Request> requests;
+    @ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST,  CascadeType.REFRESH })
+    @JoinTable(name = "product_offer",
+            joinColumns = {@JoinColumn(name = "product_id")},
+            inverseJoinColumns = {@JoinColumn(name = "offer_id")})
+    private List<Offer> offers;
 }
